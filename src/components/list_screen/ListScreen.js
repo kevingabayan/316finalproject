681df3207662664b271a593580cc8493df692604
kevin.gabayan <kevin.gabayan@stonybrook.edu>
2019-12-11 17:22:25 -0500

    okzoomer
    Added zooming in, zooming out, and disabled the save button

--- src/components/list_screen/ListScreen.js
+++ src/components/list_screen/ListScreen.js
@@ -3,20 +3,32 @@ import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
+import {Button, Icon} from 'react-materialize';
 import { getFirestore } from 'redux-firestore';
 import { withRouter} from 'react-router-dom';
 import Draggable from 'react-draggable';
-import ScrollArea from 'react-scrollbar';
 
 class ListScreen extends Component {
+    
     state = {
+        width: this.props.wireFrame.width,
+        height: this.props.wireFrame.height,
+        zoom: 100,
+        changepresent: "disabled"
+    }
+    okzoomerin = () => {
+        this.setState({zoom: this.state.zoom * 2});
+    }
+    okzoomerout = () => {
+        this.setState({zoom: this.state.zoom * 1/2});
     }
     render() {
         const auth = this.props.auth;
-        const wireFrames = this.props.wireFrames;
-        const containerStyle = {
-            width: '5000px',
-            height: '5000px',
+        const wireFrameStyle = {
+            width: this.state.width,
+            height: this.state.height,
+            zoom: this.state.zoom.toString() + "%",
+            borderStyle: 'solid'
         }
         if (!auth.uid) {
             return <Redirect to="/" />;
@@ -25,12 +37,31 @@ class ListScreen extends Component {
             <div class = "row">
                 <div class = "col s2">
                     <div class="edit-card card-panel teal">
+
+                        <div class = "row">
+                            <div class = "col s6">
+                                <a class={"waves-effect waves-light red btn " + this.state.changepresent}>Save</a>
+                            </div>
+                            <div class = "col s6">
+                                <a class="waves-effect waves-light red btn">Close</a>
+                            </div>
+                        </div>
+
+                        <div class = "row">
+                            <div class = "col s6">
+                                <a class="btn btn waves-effect waves-light red" onClick = {this.okzoomerin}><i class="material-icons">zoom_in</i></a>
+                            </div>
+                            <div class = "col s6">
+                                <a class="btn btn waves-effect waves-light red"onClick = {this.okzoomerout}><i class="material-icons">zoom_out</i></a>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
                 <div class = "col s8">
                     <div class = "edit-card-main card-panel white">
-                        <div className = "BORED" style = {containerStyle}>
-                            ah yes
+                        <div className = "BORED" style = {wireFrameStyle}>
+                            
                         </div>
                     </div>
                 </div>
@@ -44,6 +75,7 @@ class ListScreen extends Component {
         );
     }
 }
+
 const mapStateToProps = (state, ownProps) => {
   const { id } = ownProps.match.params;
   const { wireFrames } = state.firestore.data;
